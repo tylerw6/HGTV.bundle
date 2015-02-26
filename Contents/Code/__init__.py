ce370ab1bf95cb2f253f32cafd1cf5a36e831335
Sander <sander@plex.tv>
2015-02-26 01:23:17 +0100

    Ad PlayVideo function to reduce the number of http requests when building a list of videos.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,11 +1,11 @@
-NAME = "HGTV"
+NAME = 'HGTV'
 PREFIX = '/video/hgtv'
-BASE_URL = "http://www.hgtv.com"
+BASE_URL = 'http://www.hgtv.com'
 
-FULLEP_URL = "http://www.hgtv.com/shows/full-episodes"
-SHOW_LINKS_URL = "http://www.hgtv.com/shows/shows-a-z"
+FULLEP_URL = 'http://www.hgtv.com/shows/full-episodes'
+SHOW_LINKS_URL = 'http://www.hgtv.com/shows/shows-a-z'
 
-NAMESPACES = {"a":"http://www.w3.org/2005/SMIL21/Language"}
+SMIL_NS = {'a': 'http://www.w3.org/2005/SMIL21/Language'}
 
 ####################################################################################################
 def Start():
@@ -18,9 +18,12 @@ def Start():
 def MainMenu():
 
     oc = ObjectContainer()
-    oc.add(DirectoryObject(key = Callback(FullEpMenu, title="Full Episodes"), title="Full Episodes"))
-    oc.add(DirectoryObject(key = Callback(Alphabet, title="All HGTV Shows"), title="All HGTV Shows"))
+
+    oc.add(DirectoryObject(key = Callback(FullEpMenu, title='Full Episodes'), title='Full Episodes'))
+    oc.add(DirectoryObject(key = Callback(Alphabet, title='All HGTV Shows'), title='All HGTV Shows'))
+
     return oc
+
 ####################################################################################################
 # This function produces a list of shows from the HGTV full episodes page
 @route(PREFIX + '/fullepmenu')
@@ -29,49 +32,65 @@ def FullEpMenu(title):
     oc = ObjectContainer(title2=title)
 
     for item in HTML.ElementFromURL(FULLEP_URL).xpath('//div[@class="parbase editorialPromo section"]//ul/li'):
+
         title = item.xpath('.//h4/a/text()')[0]
-        url = item.xpath('./div[@class="media"]/a/@href')[0]
+        summary = item.xpath('.//h4/a/span//text()')[0]
         thumb = item.xpath('./div[@class="media"]/a/img/@src')[0]
-        desc = item.xpath('.//h4/a/span//text()')[0]
-        oc.add(DirectoryObject(key = Callback(VideoBrowse, url=url, title=title), title=title, thumb=thumb, summary=desc))
+        url = item.xpath('./div[@class="media"]/a/@href')[0]
+
+        oc.add(DirectoryObject(
+            key = Callback(VideoBrowse, url=url, title=title),
+            title = title,
+            summary = summary,
+            thumb = Resource.ContentsOfURLWithFallback(url=thumb)
+        ))
 
     # sort shows in alphabetical order here
-    oc.objects.sort(key = lambda obj: obj.title)
+    oc.objects.sort(key=lambda obj: obj.title)
 
     if len(oc) < 1:
-        return ObjectContainer(header="Empty", message="There are no full episode shows to list")
+        return ObjectContainer(header='Empty', message='There are no full episode shows to list')
     else:
         return oc
+
 ####################################################################################################
 # A to Z pull for HGTV shows
 @route(PREFIX + '/alphabet')
 def Alphabet(title):
+
     oc = ObjectContainer(title2=title)
-    for ch in HTML.ElementFromURL(SHOW_LINKS_URL).xpath('//section[@class="site-index"]/h2//text()'):
-        title = ch
-        ch = ch.lower()
-        oc.add(DirectoryObject(key=Callback(AllShows, title=title, ch=ch), title=title))
+
+    for char in HTML.ElementFromURL(SHOW_LINKS_URL).xpath('//section[@class="site-index"]/h2//text()'):
+
+        oc.add(DirectoryObject(key=Callback(AllShows, char=char), title=char))
     
     if len(oc) < 1:
-        return ObjectContainer(header="Empty", message="There are no shows to list")
+        return ObjectContainer(header='Empty', message='There are no shows to list')
     else:
         return oc
+
 ####################################################################################################
 # This function produces a list of all HGTV Shows based on letter chosen in Alphabet function
 @route(PREFIX + '/allshows')
-def AllShows(title, ch):
+def AllShows(char):
 
-    oc = ObjectContainer(title2=title)
+    oc = ObjectContainer(title2=char)
 
-    for show in HTML.ElementFromURL(SHOW_LINKS_URL).xpath('//h2[@id="%s"]/following-sibling::ul/li/a' %ch):
+    for show in HTML.ElementFromURL(SHOW_LINKS_URL).xpath('//h2[@id="%s"]/following-sibling::ul/li/a' % (char.lower())):
+
         title = show.text
-        show_url = show.xpath("./@href")[0]
-        oc.add(DirectoryObject(key = Callback(GetVideoLinks, show_url=show_url, title=title), title = title))
+        show_url = show.xpath('./@href')[0]
+
+        oc.add(DirectoryObject(
+            key = Callback(GetVideoLinks, show_url=show_url, title=title),
+            title = title
+        ))
 
     if len(oc) < 1:
-        return ObjectContainer(header="Empty", message="There are no shows to list")
+        return ObjectContainer(header='Empty', message='There are no shows to list')
     else:
         return oc
+
 ####################################################################################################
 # This function pulls the video and full episode links from a show's main page
 @route(PREFIX + '/getvideolinks')
@@ -79,122 +98,129 @@ def GetVideoLinks(title, show_url):
 
     oc = ObjectContainer(title2=title)
     data = HTML.ElementFromURL(show_url)
+
     try:
-        video_url = data.xpath('//div[@class="sub-navigation"]//a[contains(text(), "Videos")]//@href')[0]
-        oc.add(DirectoryObject(key = Callback(VideoBrowse, url=video_url, title="Videos"), title="Videos"))
+        video_url = data.xpath('//div[@class="sub-navigation"]//a[contains(text(), "Videos")]/@href')[0]
+
+        oc.add(DirectoryObject(
+            key = Callback(VideoBrowse, url=video_url, title='Videos'),
+            title = 'Videos'
+        ))
+
     except:
         pass
+
     # there can be more than one full episode link here if there are multiple seasons so make it a list and loop thru
     try:
         fullep_list = data.xpath('//div[@class="sub-navigation"]//a[contains(text(), "Full Episodes")]')
+
         for item in fullep_list:
-            fullep_url = item.xpath('.//@href')[0]
+
+            fullep_url = item.xpath('./@href')[0]
             full_title = item.xpath('.//text()')[0]
-            oc.add(DirectoryObject(key = Callback(VideoBrowse, url=fullep_url, title=full_title), title=full_title))
+
+            oc.add(DirectoryObject(
+                key = Callback(VideoBrowse, url=fullep_url, title=full_title),
+                title = full_title
+            ))
+
     except:
         pass
-        
+
     if len(oc) < 1:
-        return ObjectContainer(header="No Videos", message="This show does not have a video page")
+        return ObjectContainer(header='No Videos', message='This show does not have a video page')
     else:
         return oc
+
 ####################################################################################################
 # This function produces a list of videos for any page with a video player in it 
 @route(PREFIX + '/videobrowse')
-def VideoBrowse(url, title = None):
+def VideoBrowse(url, title):
 
     oc = ObjectContainer(title2=title)
     page = HTML.ElementFromURL(url)
-    
+
     # To prevent any issues with URLs that do not contain the video playlist json, we put the json pull in a try/except
     try:
         json_data = page.xpath('//div[@class="video-player-container"]/@data-video-prop')[0]
         json = JSON.ObjectFromString(json_data)
-    except: json = None
-    
-    if json:
-        for video in json['channels'][0]['videos']:
-            vid_smil = video['releaseUrl']
+    except:
+        json = None
 
-            if not vid_smil.startswith('http://'):
+    if json:
+
+        for video in json['channels'][0]['videos']:
+
+            smil_url = video['releaseUrl']
+
+            if not 'link.theplatform.com' in smil_url:
                 continue
 
             title = video['title'].replace('&amp,', '&')
+            summary = video['description']
             duration = int(video['length'])*1000
-            desc = video['description']
             thumb = BASE_URL + video['thumbnailUrl']
 
             oc.add(
-                CreateObject(
-                    url = url,
-                    vid_smil = vid_smil,
+                CreateVideoClipObject(
+                    smil_url = smil_url,
                     title = title,
+                    summary = summary,
                     duration = duration,
-                    summary = desc,
                     thumb = thumb
                 )
             )
+
     else:
-        Log('%s does not contain a video list json or the json is incomplete' %url)
+        Log('%s does not contain a video list json or the json is incomplete' % (url))
 
     if len(oc) < 1:
-        Log ('still no value for objects')
-        return ObjectContainer(header="Empty", message="There are currently no videos for this show.")
+        return ObjectContainer(header='Empty', message='There are currently no videos for this show')
     else:
         return oc
 
 ####################################################################################################
-# This function creates an object container for RSS feeds that have a media file in the feed
-@route(PREFIX + '/createobject', duration=int)
-def CreateObject(url, vid_smil, title, duration, thumb, summary, include_container=False):
+@route(PREFIX + '/createvideoclipobject', duration=int, include_container=bool)
+def CreateVideoClipObject(smil_url, title, summary, duration, thumb, include_container=False):
 
-    smil = XML.ElementFromURL(vid_smil)
-    res_list = []
-    
-    for videos in smil.xpath('//a:video', namespaces=NAMESPACES):
-        vid_source = videos.xpath('./@src', namespaces=NAMESPACES)[0]
-        # All videos appear to be mp4 but do a check to make sure
-        vid_type = vid_source.split('.')[-1]
-        if vid_type != 'mp4':
-            Log('The %s video is of type %s and not compatible with this plugin' %(vid_source, vid_type))
-            continue
-        vid_resolution = videos.xpath('./@height', namespaces=NAMESPACES)[0]
-        # The smil breaks the video up into parts to include ads but every part references the same mp4 file just at different start and end points
-        # Since there are multiple occurrences of each source and resolution, we get the first one and then break out
-        if (vid_source, vid_resolution) not in res_list:
-            res_list.append((vid_source, vid_resolution))
-        else:
-            break
-    #Log('This value of res_list is %s' %res_list)
-        
-    # This makes sure that the smil returned at least one value for the video and resolution
-    if len(res_list)==0:
-        Log ('no values for resolution and video source')
-        return ObjectContainer(header="Empty", message="The video information associated with this webpage is incomplete or invalid.")
-    else:
-        # A few videos mention the season or episode but most do not so since this is an internal feature, we are just making them all VideoClipObjects
-        new_object = VideoClipObject(
-            key = Callback(CreateObject, url=url, vid_smil=vid_smil, title=title, duration=duration, thumb=thumb, summary=summary, include_container=True),
-            rating_key = vid_source,
-            title = title,
-            summary = summary,
-            thumb = Resource.ContentsOfURLWithFallback(url=thumb),
-            duration = duration,
-            items = [
-                MediaObject(
-                    parts = [
-                        PartObject(key=source)
-                            ],
-                            container = Container.MP4,
-                            video_codec = VideoCodec.H264,
-                            audio_codec = AudioCodec.AAC,
-                            audio_channels = 2,
-                            video_resolution = resolution
-                )for source,resolution in res_list
-            ]
-        )
+    videoclip_obj = VideoClipObject(
+        key = Callback(CreateVideoClipObject, smil_url=smil_url, title=title, summary=summary, duration=duration, thumb=thumb, include_container=True),
+        rating_key = smil_url,
+        title = title,
+        summary = summary,
+        duration = duration,
+        thumb = Resource.ContentsOfURLWithFallback(url=thumb),
+        items = [
+            MediaObject(
+                parts = [
+                    PartObject(key=Callback(PlayVideo, smil_url=smil_url, resolution=resolution))
+                ],
+                container = Container.MP4,
+                video_codec = VideoCodec.H264,
+                audio_codec = AudioCodec.AAC,
+                audio_channels = 2,
+                video_resolution = resolution
+            ) for resolution in [720, 540, 480]
+        ]
+    )
 
     if include_container:
-        return ObjectContainer(objects=[new_object])
+        return ObjectContainer(objects=[videoclip_obj])
     else:
-        return new_object
+        return videoclip_obj
+
+####################################################################################################
+@route(PREFIX + '/playvideo', resolution=int)
+@indirect
+def PlayVideo(smil_url, resolution):
+
+    xml = XML.ElementFromURL(smil_url)
+    available_versions = xml.xpath('//a:switch[1]/a:video/@height', namespaces=SMIL_NS)
+
+    if len(available_versions) < 1:
+        raise Ex.MediaNotAvailable
+
+    closest = min((abs(int(resolution) - int(i)), i) for i in available_versions)[1]
+    video_url = xml.xpath('//a:switch[1]/a:video[@height="%s"]/@src' % closest, namespaces=SMIL_NS)[0]
+
+    return IndirectResponse(VideoClipObject, key=video_url)
